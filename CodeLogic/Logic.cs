6b5e0c59e1fbe3926df44722c0a34400c96dd560
Arthur-Iberius <arthur.kaiszer@gmail.com>
2025-07-14 19:24:35 -0300

    Add project files.

---
+++ CodeLogic/Logic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace LogAnalyzer.CodeLogic
+{
+    public class Logic
+    {
+        public static void LogAnalizerLogic()
+        {
+            Debug.WriteLine("Hello");
+        }
+    }
+}
