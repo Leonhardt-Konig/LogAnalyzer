da503c523ce4ed9daf0fc2ece06fd6be4e0b83ae
Arthur-Iberius <arthur.kaiszer@gmail.com>
2025-07-16 18:33:16 -0300

    Initial implementation of text rendering, will be optimized.

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.IO;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,29 +20,45 @@ namespace LogAnalyzer.Views
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     
-
+    //TODO: OPTIMIZE THE RENDERING, DON'T LOAD STRINGS TO RAM.
     public partial class MainWindow : Window
 
     {
-        /*public ObservableCollection<string> LogEntries { get; set; } = new ObservableCollection<string>();
-        foreach (var line in loglines) 
+        private string? filePath = null;
+        public ObservableCollection<string> LogEntries { get; set; } = new ObservableCollection<string>();
+        public async Task FindFirstLines(List<long> OffSetList)
         {
-            LogEntries.Add(line);
-        }*/
-        string? filePath = null;
-        public void OpenMenuItem_Click(object sender, RoutedEventArgs e)
+            using var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            using var sr = new StreamReader(fs);
+            foreach (var offset in OffSetList)
+            {
+                //seek to line and read it, add string to Observable collection
+                fs.Seek(offset, SeekOrigin.Begin);
+                string? line = await sr.ReadLineAsync();
+                if (line != null)
+                {
+                    LogEntries.Add(line);
+                };
+            }
+            
+        }
+        
+        public async void OpenMenuItem_Click(object sender, RoutedEventArgs e)
         {
-                filePath = Logic.GetWorkFile();
+            filePath = Logic.GetWorkFile();
+            await FindFirstLines(Logic.GetByteOffSets(filePath: filePath));
         }
 
         public void CloseMenuItem_Click(object sender, RoutedEventArgs e)
         {
             //add a method to empty file path.
             Logic.PrintPath(filePath);
+            
         } 
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
             //Logic.LogAnalizerLogic();
             //Need to get the offsets.
         }
